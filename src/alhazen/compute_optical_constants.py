49b2364cae854e38686f9f1cbd6e571cf1b1a949
giovanni-angeli <giovanni.angeli6@gmail.com>
2022-09-22 16:06:05 +0200

    templates

--- src/alhazen/compute_optical_constants.py
+++ src/alhazen/compute_optical_constants.py
@@ -9,20 +9,24 @@ import math
 
 def compute_R(json_structure, params):
 
+    fraction1 = json_structure.get('structure', {}).get("layer1", {}).get("fraction1", 30)
+    fraction1 = float(fraction1)
     thickness_active_layer = params.get('thickness_active_layer', 0)
     thickness_active_layer = float(thickness_active_layer) * .001
     def f(i):
-        return 50. * (0.5 + 0.5 * math.cos(thickness_active_layer * i)) + 5. * random.random()
+        return fraction1 * (0.5 + 0.5 * math.cos(thickness_active_layer * i)) + 5. * random.random()
 
     out_data = [(i, f(i)) for i in range(270, 1100, 2)]
 
     return out_data 
 def compute_T(json_structure, params):
 
+    fraction1 = json_structure.get('structure', {}).get("layer1", {}).get("fraction1", 30)
+    fraction1 = float(fraction1)
     thickness_active_layer = params.get('thickness_active_layer', 0)
     thickness_active_layer = float(thickness_active_layer) * .001
     def f(i):
-        return 50. * (0.5 + 0.5 * math.sin(thickness_active_layer * i)) + 5. * random.random()
+        return fraction1 * (0.5 + 0.5 * math.sin(thickness_active_layer * i)) + 5. * random.random()
 
     out_data = [(i, f(i)) for i in range(270, 1100, 2)]
 
