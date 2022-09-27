b6b9b4705f9fa0746b6b01517fc2cae62d2a0c32
giovanni-angeli <giovanni.angeli6@gmail.com>
2022-09-27 17:02:39 +0200

    allow empty structure and empty measure

--- src/alhazen/frontend.py
+++ src/alhazen/frontend.py
@@ -98,8 +98,8 @@ class Index(BaseRequestHandler):  # pylint: disable=too-few-public-methods
 
     def get(self):
 
-        structure_file_list = os.listdir(STRUCTURE_FILES_PATH)
-        measure_file_list = os.listdir(MEASURE_FILES_PATH)
+        structure_file_list = [''] + os.listdir(STRUCTURE_FILES_PATH)
+        measure_file_list = [''] + os.listdir(MEASURE_FILES_PATH)
 
         ctx = {
             'page_name': 'Alhazen graph page',
@@ -191,30 +191,26 @@ class Frontend(tornado.web.Application):
         except BaseException:  # pylint: disable=broad-except
             logging.error(traceback.format_exc())
 
-    async def refresh_file_lists(self, ws_socket, params):
-        structure_file_list = os.listdir(STRUCTURE_FILES_PATH)
-        measure_file_list = os.listdir(MEASURE_FILES_PATH)
-        structure_file_list.sort()
-        measure_file_list.sort()
-        for name, list_, target in (
-                ('structure', structure_file_list, 'structure_file_list'),
-                ('measure', measure_file_list, 'measure_file_list')):
+    async def send_message_to_UI(self, element_id, payload, target='innerHTML', class_name=None, ws_socket=None):
 
-            html_ = f"<h3>{name} files:</h3>"
-            for n, f in enumerate(list_):
-                col = "#F7FAF7" if n % 2 else "#EEEEEE"
-                html_ += f"""
-<div class="row" style="background-color:{col};">
-<span class="col-7">{str(f)}</span>\n
-<span class="small_btn col-1" onclick="on_template_clicked('edit',   '{str(name)}', '{str(f)}')">edit  </span>
-<span class="small_btn col-1" onclick="on_template_clicked('delete', '{str(name)}', '{str(f)}')">delete</span>
-<span class="small_btn col-1" onclick="on_template_clicked('clone',  '{str(name)}', '{str(f)}')">clone </span>
-</div>
-"""
+        msg = {
+            "element_id": element_id,
+            'target': target,
+            'payload': payload,
+            'class_name': class_name}
 
-            await self.send_message_to_UI(target, html_, ws_socket)
+        msg = json.dumps(msg)
 
-    async def refresh_data_graph(self, ws_socket, params):
+        if ws_socket:
+            await ws_socket.write_message(msg)
+
+        else:  # broadcast
+            for ws_ch in self.web_socket_channels:
+                await ws_ch.write_message(msg)
+
+    async def refresh_data_graph(self, params, ws_socket):
+
+        await self.send_message_to_UI("status_display", 'recalculating model, please wait...', ws_socket=ws_socket)
 
         self.backend.load_structure()
         self.backend.load_measure()
@@ -246,30 +242,40 @@ class Frontend(tornado.web.Application):
                 label, serie = line
                 line_chart.add(label, serie)
 
-            # ~ graph_svg = line_chart.render(is_unicode=True)
-            # ~ await self.send_message_to_UI("pygal_data_container", innerHTML=None, ws_client=ws_socket, data=graph_svg)
-
             STATIC_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'static')
             line_chart.render_to_file(STATIC_PATH + '/temp_chart.svg')
-            await self.send_message_to_UI("pygal_data_container", payload='/static/temp_chart.svg', ws_client=ws_socket, target='data')
+            await self.send_message_to_UI("pygal_data_container",
+                                          payload='/static/temp_chart.svg', target='data', ws_socket=ws_socket)
 
-            await self.send_message_to_UI("pygal_description_container", message, ws_socket)
+            await self.send_message_to_UI("pygal_description_container", message, ws_socket=ws_socket)
 
-    async def send_message_to_UI(self, element_id, payload, ws_client=None, target='innerHTML', class_name=None):
+        _msg = f"structure_file:{self.backend.structure_file}"
+        _msg += f", measure_file:{self.backend.measure_file}"
+        await self.send_message_to_UI("status_display", f"done.<br/>{_msg}", ws_socket=ws_socket)
 
-        msg = {"element_id": element_id, 'target': target, 'payload': payload, 'class_name': class_name}
-        msg = json.dumps(msg)
+    async def refresh_file_lists(self, ws_socket):
 
-        if ws_client:
-            await ws_client.write_message(msg)
-            # ~ t_ = ws_client.write_message(msg)
-            # ~ asyncio.ensure_future(t_)
+        structure_file_list = os.listdir(STRUCTURE_FILES_PATH)
+        measure_file_list = os.listdir(MEASURE_FILES_PATH)
+        structure_file_list.sort()
+        measure_file_list.sort()
+        for name, list_, element_id in (
+                ('structure', structure_file_list, 'structure_file_list'),
+                ('measure', measure_file_list, 'measure_file_list')):
 
-        else:  # broadcast
-            for ws_ch in self.web_socket_channels:
-                await ws_ch.write_message(msg)
-                # ~ t_ = ws_ch.write_message(msg)
-                # ~ asyncio.ensure_future(t_)
+            html_ = f"<h3>{name} files:</h3>"
+            for n, f in enumerate(list_):
+                col = "#F7FAF7" if n % 2 else "#EEEEEE"
+                html_ += f"""
+<div class="row" style="background-color:{col};">
+<span class="col-7">{str(f)}</span>\n
+<span class="small_btn col-1" onclick="on_template_clicked('edit',   '{str(name)}', '{str(f)}')">edit  </span>
+<span class="small_btn col-1" onclick="on_template_clicked('delete', '{str(name)}', '{str(f)}')">delete</span>
+<span class="small_btn col-1" onclick="on_template_clicked('clone',  '{str(name)}', '{str(f)}')">clone </span>
+</div>
+"""
+
+            await self.send_message_to_UI(element_id, html_, ws_socket=ws_socket)
 
     async def handle_message_from_UI(self, ws_socket, message):
 
@@ -277,101 +283,97 @@ class Frontend(tornado.web.Application):
         message_dict = json.loads(message)
         try:
 
-            if message_dict.get("command") == "install_templates":
-
-                self.backend.install_templates()
-                await self.refresh_file_lists(ws_socket, params={})
-
-            elif message_dict.get("command") == "refresh_file_lists":
-
+            if message_dict.get("command"):
+                command = "_cmd_" + message_dict["command"]
                 params = message_dict.get("params", {})
-                await self.refresh_file_lists(ws_socket, params=params)
-
-            elif message_dict.get("command") == "save_template":
-
-                _type = message_dict.get("params", {}).get('type')
-                _file_name = message_dict.get("params", {}).get('file_name')
-                _file_content = message_dict.get("params", {}).get('file_content')
-                logging.debug(f"message_dict:{message_dict}"[:200])
-                _pth = STRUCTURE_FILES_PATH if _type == 'structure' else MEASURE_FILES_PATH
-                if _type == 'structure':
-                    _file_content = json.dumps(json.loads(_file_content), indent=2)
-                else:
-                    pass
-
-                with open(os.path.join(_pth, _file_name), 'w', encoding='utf-8') as f:
-                    f.write(_file_content)
-
-                await self.refresh_file_lists(ws_socket, params={})
-                await self.send_message_to_UI("status_display", f"file {_file_name} saved.", ws_socket)
-
-            elif message_dict.get("command") == "on_template_clicked":
-
-                _action = message_dict.get("params", {}).get('action')
-                _type = message_dict.get("params", {}).get('type')
-                _file_name = message_dict.get("params", {}).get('file_name')
-                _file_content = message_dict.get("params", {}).get('file_content')
-
-                logging.debug(f"message_dict:{message_dict}"[:200])
-
-                if _action and _type and _file_name:
-                    if _action == 'delete':
-                        _pth = STRUCTURE_FILES_PATH if _type == 'structure' else MEASURE_FILES_PATH
-                        r = os.remove(os.path.join(_pth, _file_name))
-                        logging.warning(f"r:{r}")
-
-                    elif _action == 'edit':
-                        if _type == 'structure':
-                            with open(os.path.join(STRUCTURE_FILES_PATH, _file_name), encoding='utf-8') as f:
-                                _content = f.read()
-                                _content = json.dumps(json.loads(_content), indent=2)
-                                # ~ _content = "<br/>".join(_content.split('\n'))
-                        else:
-                            with open(os.path.join(MEASURE_FILES_PATH, _file_name), encoding='utf-8') as f:
-                                _content = f.read()
-                                # ~ _content = "<br/>".join(_content.split('\n'))
-
-                        await self.send_message_to_UI("view_file_name", _file_name, ws_socket, class_name="view_file_container")
-                        await self.send_message_to_UI("view_file_type", _type, ws_socket)
-                        await self.send_message_to_UI("view_file_content", _content, ws_socket)
-
-                    elif _action == 'clone':
-                        _pth = STRUCTURE_FILES_PATH if _type == 'structure' else MEASURE_FILES_PATH
-                        original = os.path.join(_pth, _file_name)
-                        target = '.cpy'.join(os.path.splitext(original))
-
-                        shutil.copyfile(original, target)
-
-                    await self.refresh_file_lists(ws_socket, params={})
-
-            elif message_dict.get("command") == "structure_selected":
-
-                _name = message_dict.get("params")
-                self.backend.load_structure(_name)
-
-            elif message_dict.get("command") == "measure_selected":
-
-                _name = message_dict.get("params")
-                self.backend.load_measure(_name)
-
-            elif message_dict.get("command") == "refresh_data_graph":
-
-                params = message_dict.get("params", {})
-                await self.send_message_to_UI("status_display", 'recalculating model, please wait...', ws_socket)
-                await self.refresh_data_graph(ws_socket, params=params)
-                _msg = f"structure_file:{self.backend.structure_file}"
-                _msg += f", measure_file:{self.backend.measure_file}"
-                await self.send_message_to_UI("status_display", f"done.<br/>{_msg}", ws_socket)
+                await getattr(self, command)(params, ws_socket)
 
             else:
                 answer = f"received:{message}"
                 innerHTML = "ws_index:{} [{}] {} -> {}".format(
                     self.web_socket_channels.index(ws_socket), time.asctime(), message, answer)
-                await self.send_message_to_UI("status_display", innerHTML, ws_socket)
+                await self.send_message_to_UI("status_display", innerHTML, ws_socket=ws_socket)
                 logging.warning(f"innerHTML:{innerHTML}")
-
-            # ~ await self.send_message_to_UI("error_display", "-", ws_socket)
 
         except Exception as e:
             logging.warning(traceback.format_exc())
-            await self.send_message_to_UI("error_display", f"Exception:{e}", ws_socket)
+            await self.send_message_to_UI("error_display", f"Exception:{e}", ws_socket=ws_socket)
+
+    async def _cmd_refresh_file_lists(self, params, ws_socket):  # pylint: disable=unused-private-member, unused-argument
+
+        await self.refresh_file_lists(ws_socket)
+
+    async def _cmd_on_template_clicked(self, params, ws_socket):  # pylint: disable=unused-private-member
+
+        _action = params.get('action')
+        _type = params.get('type')
+        _file_name = params.get('file_name')
+        _file_content = params.get('file_content')
+
+        logging.debug(f"params:{params}"[:200])
+
+        if _action and _type and _file_name:
+            if _action == 'delete':
+                _pth = STRUCTURE_FILES_PATH if _type == 'structure' else MEASURE_FILES_PATH
+                r = os.remove(os.path.join(_pth, _file_name))
+                logging.warning(f"r:{r}")
+
+            elif _action == 'edit':
+                if _type == 'structure':
+                    with open(os.path.join(STRUCTURE_FILES_PATH, _file_name), encoding='utf-8') as f:
+                        _content = f.read()
+                        _content = json.dumps(json.loads(_content), indent=2)
+                        # ~ _content = "<br/>".join(_content.split('\n'))
+                else:
+                    with open(os.path.join(MEASURE_FILES_PATH, _file_name), encoding='utf-8') as f:
+                        _content = f.read()
+                        # ~ _content = "<br/>".join(_content.split('\n'))
+
+                await self.send_message_to_UI("view_file_name", _file_name, class_name="view_file_container", ws_socket=ws_socket)
+                await self.send_message_to_UI("view_file_type", _type, ws_socket=ws_socket)
+                await self.send_message_to_UI("view_file_content", _content, ws_socket=ws_socket)
+
+            elif _action == 'clone':
+                _pth = STRUCTURE_FILES_PATH if _type == 'structure' else MEASURE_FILES_PATH
+                original = os.path.join(_pth, _file_name)
+                target = '.cpy'.join(os.path.splitext(original))
+
+                shutil.copyfile(original, target)
+
+            await self.refresh_file_lists(ws_socket)
+
+    async def _cmd_structure_selected(self, params, ws_socket):  # pylint: disable=unused-private-member, unused-argument
+        _name = params
+        self.backend.load_structure(_name)
+
+    async def _cmd_measure_selected(self, params, ws_socket):  # pylint: disable=unused-private-member, unused-argument
+
+        _name = params
+        self.backend.load_measure(_name)
+
+    async def _cmd_install_templates(self, params, ws_socket):  # pylint: disable=unused-private-member, unused-argument
+
+        self.backend.install_templates()
+        await self.refresh_file_lists(ws_socket)
+
+    async def _cmd_refresh_data_graph(self, params, ws_socket):  # pylint: disable=unused-private-member
+
+        await self.refresh_data_graph(params, ws_socket)
+
+    async def _cmd_save_template(self, params, ws_socket):  # pylint: disable=unused-private-member
+
+        _type = params.get('type')
+        _file_name = params.get('file_name')
+        _file_content = params.get('file_content')
+        logging.debug(f"params:{params}"[:200])
+        _pth = STRUCTURE_FILES_PATH if _type == 'structure' else MEASURE_FILES_PATH
+        if _type == 'structure':
+            _file_content = json.dumps(json.loads(_file_content), indent=2)
+        else:
+            pass
+
+        with open(os.path.join(_pth, _file_name), 'w', encoding='utf-8') as f:
+            f.write(_file_content)
+
+        await self.refresh_file_lists(ws_socket)
+        await self.send_message_to_UI("status_display", f"file {_file_name} saved.", ws_socket=ws_socket)
