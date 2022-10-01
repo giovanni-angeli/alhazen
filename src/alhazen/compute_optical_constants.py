b6a3d952015738ef889f8091f9b94e7e5489dddf
Alberto Maurizi <alberto.maurizi@cnr.it>
2022-10-01 11:01:55 +0200

    adapted to use 'form' not 'class' for panels of editable params

--- src/alhazen/compute_optical_constants.py
+++ src/alhazen/compute_optical_constants.py
@@ -12,7 +12,7 @@ import numpy as np
 
 # local modules from "optical"
 import alhazen.optical_functions
-# ~ import alhazen.ScatteringMatrix
+import alhazen.ScatteringMatrix
 
 DEFAULT_NP = 400
 
@@ -24,48 +24,55 @@ def compute_RT(json_structure, params):
 
     logging.info(f"params:{params}")
 
-    # esempio di estrazione dati da struttura
     # structure is a list of layers; each layer is a dict that includes a
     # list of materials; each material is a dict;
-
-    # examples:
-    # layer = json_structure.get('layer') # get the list of layers; each layer is a dict
-    # json_structure.get('layer')[0].get('material')[0].get('fname') # get fname of first material of first layer
-
     layers = json_structure.get('layers') # get the list of layers; each layer is a dict
 
     # wl range
-    wl_np = int( params.get('wl_np', DEFAULT_NP) )
-    wl_min,wl_max = [ float(a) for a in params.get('wl_range').split(',') ]
+    wl_min,wl_max = [ float(a) for a in params.get('plot_edit_panel').get('wl_range').split(',') ]
     # TODO: verify wl_min < wl_max
+
+    # TODO:
     #if autoRange:
+    #   wl_min = min wl value read from structure refractiveindex file
+    #   wl_max = max wl value read from structure refractiveindex file
+    #else:
+    #   wl_min,wl_max = [ float(a) for a in params.get('wl_range').split(',') ]
     #   wl_min = max( wl_min, ...) # TODO: put the min wl value read from structure refractiveindex file
     #   wl_max = min( wl_max, ...) # TODO: put the max wl value read from structure refractiveindex file
-    wl_range = np.linspace( wl_min, wl_max, wl_np )
+    wl_np = int( params.get('plot_edit_panel').get('wl_np', DEFAULT_NP) )
+    wl = np.linspace( wl_min, wl_max, wl_np )
 
-    # TODO: process structure to prepare input to ScatteringMatrix.compute_RT
+    # TODO: prepare structure  as input to ScatteringMatrix.compute_RT
     structure = []
-    for l in layers:
-        thickness = float( l.get('thickness') )
-        #coherence = bool( l.get('coherence') )
-        coherence = abs( 1 - int(l.get('coherence')) ) # this is to make it compatible with "optical" functions
-        roughness = float( l.get('roughness') )
-        materials = l.get('materials')
+    for layer in layers:
+        thickness = float( layer.get('thickness') )
+        #coherence = bool( layer.get('coherence') )
+        incoherence = abs( 1 - int(layer.get('coherence')) ) # this is to make it compatible with "optical" functions that use incoherence (!?)
+        roughness = float( layer.get('roughness') )
+        materials = layer.get('materials')
         # TODO: test sum of material fracts
-        # compute refractiveIndex (ni,nr) from materials (if one only: ... else: EMA)
-        # interpolate refractiveIndex onto wl
-        # structure.append( [thickness, refractiveIndex, coherence, roughness] )
+        #refractive_index = EMA(materials)
+        # interpolate refractive_index onto wl
+        #structure.append( [thickness, refractive_index, incoherence, roughness] )
 
     # altri parametri
-    thickness_active_layer = float( params.get('thickness_active_layer') )
+    # FIXME: forse la thickness of active layer va impostata qui come la
+    # thickness di uno dei layer calcolati sopra. come definire active layer
+    # nella struttura? chiedere Emanuele.
+    thickness_active_layer = float( params.get('model_edit_panel').get('thickness_active_layer') )
+    incidence_angle = float( params.get('model_edit_panel').get('incidence_angle') )
 
     def _R(i):
         return (0.5 + 0.5 * math.cos(thickness_active_layer * i)) + 5. * random.random()
     def _T(i):
         return (0.5 + 0.5 * math.sin(thickness_active_layer * i)) + 5. * random.random()
 
-    R = [ (i,_R(i)) for i in wl_range]
-    T = [ (i,_T(i)) for i in wl_range]
+    # ComputeRT da ScatteringMatrix.py
+    # R,T = ComputeRT(structure, wl, incidence_angle)
+
+    R = [ (i,_R(i)) for i in wl]
+    T = [ (i,_T(i)) for i in wl]
 
     return R,T
 
@@ -73,9 +80,10 @@ def compute_RT(json_structure, params):
 def get_description(_structure, params):
 
     message = f"params: {params}<br/>"
-    #message += "layers: "
-    #for k, v in _structure.get('structure', {}).items():
-    #    message += f"{k} {v.get('thickness')}(µ), "
-    #message = f"layers: {_structure.get('layers')}"
+#    message += "layers: "
+# FIXME: does not show anything
+#    for k, v in _structure.get('structure', {}).items():
+#        message += f"{k} {v.get('thickness')}(µ), "
 
     return message
+
