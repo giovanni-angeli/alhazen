e8bbe84f1b7d70b9476c7b2fd96574264c79bbd6
Alberto Maurizi <alberto.maurizi@cnr.it>
2022-10-04 15:15:42 +0200

    fully rewritten: everything delegated to optical_functions

--- src/alhazen/compute_optical_constants.py
+++ src/alhazen/compute_optical_constants.py
@@ -8,71 +8,116 @@ import random
 import math
 import logging
 
+import os
 import numpy as np
 
 # local modules from "optical"
 import alhazen.optical_functions
 import alhazen.ScatteringMatrix
 
-DEFAULT_NP = 400
+FAKE_RUN = False
 
-def compute_RT(json_structure, params):
+DEFAULT_WL_RANGE = "200,1100" # TODO: ask Emanuele for a proper value
+DEFAULT_NP = 400 # TODO: ask Emanuele for a proper value
+
+def convert_structure( json_structure, params ):
+
     '''
-    Computes Reflectance (R) and Transmittance (T) for a given structure (in
-    json format) in the range of wave length (wl) defined in params
+    translate json_structure into "opt" structure: the "multilayer" object
+    which is a list of "layer" objects, all defined in optical_functions.py
+    (from Centurioni's optical.functions) 
     '''
 
-    logging.info(f"params:{params}")
+    # get thickness of active layer from params: to be set at the thickness
+    # of active layer, if any
+    thickness_active_layer = float( params['model_edit_panel'].get('thickness_active_layer', -1) )
 
-    # structure is a list of layers; each layer is a dict that includes a
-    # list of materials; each material is a dict;
-    layers = json_structure.get('layers') # get the list of layers; each layer is a dict
+    opt_structure = alhazen.optical_functions.multilayer()
+    opt_structure.name = json_structure['name']
+    for json_layer in json_structure['layers']:
+        name = json_layer['name']
+        active = json_layer['active']
+        materials = json_layer['materials']
+        if json_layer['active'] and thickness_active_layer > 0: # user set this value different from the default which is -1
+            thickness = thickness_active_layer
+        else:
+            thickness = json_layer['thickness']
+            # FIXME: thickness_active_layer in form.input must be set to the
+            # actual thickness read from structure; TODO: ask Giovanni
+        coherence = json_layer['coherence']
+        roughness = json_layer['roughness']
 
-    # wl range
-    wl_min,wl_max = [ float(a) for a in params.get('plot_edit_panel').get('wl_range').split(',') ]
-    # TODO: verify wl_min < wl_max
+        # L'istruzione qui sotto e` ricavata da optical.functions.OpenMultilayer()
+        # Domande per Emanuele:
+        # 1. perche' alcuni valori sono trasformati in stringhe? (thickness e roughness)
+        # 2. le fractions sono numeri? NO: sono stringhe
+        # 3. incoherent e` un numero? NO: e` una stringa
+        # 4. perche; la thinckness e` /10?
+        opt_layer = alhazen.optical_functions.layer( name=name, \
+                           file1=materials[0]['fname'], file2=materials[1]['fname'],  file3=materials[2]['fname'], \
+                           fr1=str(materials[0]['fraction']), fr2=str(materials[1]['fraction']), fr3=str(materials[2]['fraction']), \
+                           thickness=str(thickness/10), incoherent="1" if coherence==0 else "0", roughness=str(roughness) )
 
-    # TODO:
-    #if autoRange:
-    #   wl_min = min wl value read from structure refractiveindex file
-    #   wl_max = max wl value read from structure refractiveindex file
-    #else:
-    #   wl_min,wl_max = [ float(a) for a in params.get('wl_range').split(',') ]
-    #   wl_min = max( wl_min, ...) # TODO: put the min wl value read from structure refractiveindex file
-    #   wl_max = min( wl_max, ...) # TODO: put the max wl value read from structure refractiveindex file
-    wl_np = int( params.get('plot_edit_panel').get('wl_np', DEFAULT_NP) )
+        opt_structure.add(opt_layer)
+
+    return opt_structure
+
+
+def wl_range( opt_structure, params ):
+    wl_min, wl_max = [ float(a) for a in params['plot_edit_panel'].get('wl_range',DEFAULT_WL_RANGE).split(',') ]
+    wl_min = max( wl_min, opt_structure.lmin )
+    wl_max = min( wl_max, opt_structure.lmax )
+    wl_np = int( params['plot_edit_panel'].get('wl_np', DEFAULT_NP) ) # FIXME: questo dovrebbe arrivare gia` come numero e invece e` una stringa!
     wl = np.linspace( wl_min, wl_max, wl_np )
 
-    # TODO: prepare structure  as input to ScatteringMatrix.compute_RT
-    structure = []
-    for layer in layers:
-        thickness = float( layer.get('thickness') )
-        #coherence = bool( layer.get('coherence') )
-        incoherence = abs( 1 - int(layer.get('coherence')) ) # this is to make it compatible with "optical" functions that use incoherence (!?)
-        roughness = float( layer.get('roughness') )
-        materials = layer.get('materials')
-        # TODO: test sum of material fracts
-        #refractive_index = EMA(materials)
-        # interpolate refractive_index onto wl
-        #structure.append( [thickness, refractive_index, incoherence, roughness] )
+    return wl
 
-    # altri parametri
-    # FIXME: forse la thickness of active layer va impostata qui come la
-    # thickness di uno dei layer calcolati sopra. come definire active layer
-    # nella struttura? chiedere Emanuele.
-    thickness_active_layer = float( params.get('model_edit_panel').get('thickness_active_layer') )
-    incidence_angle = float( params.get('model_edit_panel').get('incidence_angle') )
+def compute_RT( json_structure, params ):
 
-    def _R(i):
-        return (0.5 + 0.5 * math.cos(thickness_active_layer * i)) + 5. * random.random()
-    def _T(i):
-        return (0.5 + 0.5 * math.sin(thickness_active_layer * i)) + 5. * random.random()
+    # json_ to opt_structure
+    opt_structure = convert_structure( json_structure, params )
 
-    # ComputeRT da ScatteringMatrix.py
-    # R,T = ComputeRT(structure, wl, incidence_angle)
+    # from Centurioni's optical.functions: for each opt_structure.layer, add n,k (each with its own wl list) to the opt_structure.layer
+    alhazen.optical_functions.EMA(opt_structure)
 
-    R = [ (i,_R(i)) for i in wl]
-    T = [ (i,_T(i)) for i in wl]
+    # this find the min and max wavelenght of the whole structure and define opt_structure.lmin and opt_structure.lmax
+    alhazen.optical_functions.CheckWaveRange(opt_structure)
+
+    # Domanda per Emanuele: ci sono altre chiamate necessarie?
+
+    ## wavelength list to interpolate n,k and compute R,T
+    #wl_min, wl_max = [ float(a) for a in params['plot_edit_panel'].get('wl_range',DEFAULT_WL_RANGE).split(',') ]
+    #wl_min = max( wl_min, opt_structure.lmin )
+    #wl_max = min( wl_max, opt_structure.lmax )
+    #wl_np = int( params['plot_edit_panel'].get('wl_np', DEFAULT_NP) ) # FIXME: questo dovrebbe arrivare gia` come numero e invece e` una stringa!
+    #wl = np.linspace( wl_min, wl_max, wl_np )
+    wl = wl_range( opt_structure, params )
+
+    # get incidence angle in degrees and convert it into radians
+    incidence_angle = math.pi/180 * float( params['model_edit_panel']['incidence_angle'] ) # FIXME: questo dovrebbe arrivare gia` come numero e invece e` una stringa!
+
+    if FAKE_RUN:
+        thickness_active_layer = float( params['model_edit_panel'].get('thickness_active_layer', -1) ) # FIXME: questo dovrebbe arrivare gia` come numero e invece e` una stringa!
+        def _R(i):
+            return (0.5 + 0.5 * math.cos(thickness_active_layer * i)) + 5. * random.random()
+        def _T(i):
+            return (0.5 + 0.5 * math.sin(thickness_active_layer * i)) + 5. * random.random()
+        R = [ (i,_R(i)) for i in wl]
+        T = [ (i,_T(i)) for i in wl]
+    else:
+        Ronly,Tonly = alhazen.ScatteringMatrix.ComputeRT( alhazen.optical_functions.PrepareList(opt_structure,wl), wl, incidence_angle )
+        # WARNING: ScatteringMatrix.ComputeRT returns two numpy.ndarray with
+        # containing R and T only (not the wavelength)
+
+        # prepare output in the required format
+        R = []
+        for l,r in zip(wl,Ronly.tolist()):
+            R.append( (l,r) )
+
+        T = []
+        for l,r in zip(wl,Tonly.tolist()):
+            T.append( (l,r) )
+
 
     return R,T
 
